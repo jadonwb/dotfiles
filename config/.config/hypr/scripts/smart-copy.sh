cdca68931a5c8c0c29c197b88e21d528b501d00a
Jadon Brutcher <jadon.brutcher@deltamobile.com>
2026-01-05 15:22:05 -0600

    current state of work dots

--- config/.config/hypr/scripts/smart-copy.sh
+++ config/.config/hypr/scripts/smart-copy.sh
@@ -5,10 +5,10 @@ class=$(echo "$active" | jq -r '.class')
 # title=$(echo "$active" | jq -r '.title')
 address=$(echo "$active" | jq -r '.address')
 
-if [[ "$class" == "kitty" ]]; then
+if [[ "$class" == "com.mitchellh.ghostty" ]]; then
     pid=$(echo "$active" | jq -r '.pid')
 
-    if [[ $(ps -p "$pid" -o comm=) == *"nvim"* ]] || \
+    if [[ $(ps -p "$pid" -o comm=) == *"nvim"* ]] ||
         [[ $(ps -p "$(pgrep -P "$pid" | xargs -I {} pgrep -P {})" -o comm=) ]]; then
         hyprctl dispatch sendshortcut ",y,address:$address"
     else
