eeb176f5515a937c826925c5b3e3a8717d05666d
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-05-18 13:48:21 -0500

    push these

--- dot_config/omarchy/themes/arrowlake/neovim.lua
+++ dot_config/omarchy/themes/arrowlake/neovim.lua
@@ -1,12 +1,22 @@
 return {
   {
     'jadonwb/arrowlake.nvim',
+    opts = {
+      transparent = false,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+        popups = 'transparent',
+        statusline = 'transparent',
+      },
+      lualine_bold = true,
+      dim_inactive = false,
+    },
   },
   {
     'LazyVim/LazyVim',
     opts = {
       colorscheme = 'arrowlake-darker',
-      transparent = true,
     },
   },
 }
--- dot_config/omarchy/themes/arrowlake_light/neovim.lua
+++ dot_config/omarchy/themes/arrowlake_light/neovim.lua
@@ -1,11 +1,22 @@
 return {
-	{
-		"jadonwb/arrowlake.nvim",
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "arrowlake-light",
-		},
-	},
+  {
+    'jadonwb/arrowlake.nvim',
+    opts = {
+      transparent = false,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+        popups = 'transparent',
+        statusline = 'transparent',
+      },
+      lualine_bold = true,
+      dim_inactive = false,
+    },
+  },
+  {
+    'LazyVim/LazyVim',
+    opts = {
+      colorscheme = 'arrowlake-light',
+    },
+  },
 }
