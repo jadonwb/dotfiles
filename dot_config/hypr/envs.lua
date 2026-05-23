ca66dbdefc37180832e4dc7ed486c5a7e649b1ac
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-05-23 11:23:46 -0500

    can put overrides in ~/.local/bin

--- dot_config/hypr/envs.lua
+++ dot_config/hypr/envs.lua
@@ -37,4 +37,12 @@ if machine.is_work then
   hl.env("__GL_VRR_ALLOWED", "1")
   hl.env("OGL_DEDICATED_HW_STATE_PER_CONTEXT", "ENABLE_ROBUST")
   hl.env("WLR_DRM_NO_ATOMIC", "1")
+else
+  hl.env(
+    "PATH",
+    join_path(
+      os.getenv("HOME") .. "/.local/bin",
+      os.getenv("PATH") or "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    )
+  )
 end
