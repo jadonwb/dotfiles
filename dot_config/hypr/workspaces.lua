3cefb22d3a713da0256da5c46ec349141154c106
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-22 22:25:21 -0500

    gone

--- dot_config/hypr/workspaces.lua
+++ dot_config/hypr/workspaces.lua
@@ -7,29 +7,3 @@ hl.workspace_rule({
   workspace = "special:activity",
   on_created_empty = "omarchy-launch-tui btop",
 })
-
-hl.workspace_rule({
-  workspace = "special:youtube",
-  on_created_empty = [[omarchy-launch-webapp "https://youtube.com/"]],
-})
-
-hl.workspace_rule({
-  workspace = "special:keyboard",
-  on_created_empty = [[omarchy-launch-webapp "https://my.moergo.com/go60/##/" && omarchy-launch-webapp "https://my.moergo.com/glove80/##/"]],
-  layout = "scrolling",
-  layout_opts = { direction = "up" },
-})
-
-hl.workspace_rule({
-  workspace = "special:scratch",
-  on_created_empty = [[uwsm-app -- xdg-terminal-exec --dir="$HOME/Documents/notes" --command nvim]],
-  layout = "scrolling",
-  layout_opts = { direction = "right" },
-})
-
-hl.workspace_rule({
-  workspace = "special:media",
-  on_created_empty = [[omarchy-launch-webapp "https://www.icloud.com/" && omarchy-launch-webapp "https://www.reddit.com/" && omarchy-launch-webapp "https://music.apple.com" && vesktop]],
-  layout = "scrolling",
-  layout_opts = { direction = "right" },
-})
