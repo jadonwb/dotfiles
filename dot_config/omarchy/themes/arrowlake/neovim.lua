21b2a8bc23f8633382d1b972bd208a14216cdca1
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-07-27 11:54:48 -0500

    simplify

--- dot_config/omarchy/themes/arrowlake/neovim.lua
+++ dot_config/omarchy/themes/arrowlake/neovim.lua
@@ -1,30 +1,8 @@
 return {
-	{
-		"jadonwb/arrowlake.nvim",
-		opts = {
-			style = "darker",
-			transparent = true,
-			styles = {
-				functions = { bold = true },
-			},
-			lualine_bold = true,
-			border_style = "rounded",
-		},
-		import = "arrowlake.plugin_defaults",
-		keys = {
-			{
-				"<leader>uH",
-				function()
-					require("arrowlake").toggle_transparency()
-				end,
-				desc = "Toggle Transparecny",
-			},
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "arrowlake-darker",
-		},
-	},
+  {
+    'LazyVim/LazyVim',
+    opts = {
+      colorscheme = 'arrowlake-darker',
+    },
+  },
 }
