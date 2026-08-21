aefc6ed0278f56c75683aaf30f7f31abb0493b00
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-21 15:50:00 -0500

    wezterm tweaks

--- dot_config/hypr/bindings.lua
+++ dot_config/hypr/bindings.lua
@@ -1,16 +1,13 @@
 local float_tui = require("hypr.float-tui")
-local floats = require("hypr.floats-toggle")
 local machine = require("hypr.host")
 
 local function bind(keys, dispatcher, description)
   hl.bind(keys, dispatcher, { description = description })
 end
 
--- hl.unbind("SUPER + CTRL + T")
+-- hl.unbind("SUPER + CTRL + T") btop
 hl.unbind("SUPER + SPACE")
 hl.unbind("SUPER + ALT + SPACE")
--- hl.unbind("SUPER + SHIFT + G")
--- hl.unbind("ALT + TAB")
 
 bind("SUPER + SPACE", hl.dsp.exec_cmd("omarchy-menu"), "Omarchy menu")
 bind("SUPER + ALT + SPACE", hl.dsp.exec_cmd("omarchy-launch-walker"), "Launch apps")
@@ -20,8 +17,9 @@ bind(
   hl.dsp.exec_cmd([[uwsm-app -- xdg-terminal-exec --dir="$(omarchy-cmd-terminal-cwd)"]]),
   "Terminal"
 )
-bind("SUPER + ALT + RETURN", hl.dsp.exec_cmd("uwsm-app -- wezterm-launcher"), "Session launcher")
+
 bind("SUPER + B", hl.dsp.exec_cmd("omarchy-launch-browser"), "Browser")
+
 -- TODO: simplify, omarchy-launch-tui auto names them org.omarchy.*, so just provide a single name and the internals will make special:* and use org.omarchy.*
 bind(
   "SUPER + E",
@@ -33,7 +31,6 @@ bind(
   float_tui.new({ class = "org.omarchy.btop", special = "special:btop", launch = "omarchy-launch-tui btop" }),
   "Toggle btop"
 )
--- bind("SUPER + SHIFT + G", floats.toggle, "Toggle float drawer (hide/show floats)")
 
 if machine.is_personal then
   bind("SUPER + PERIOD", hl.dsp.exec_cmd("uwsm-app -- 1password"), "Passwords")
