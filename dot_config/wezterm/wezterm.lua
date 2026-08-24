734a9b998e6a0f8208022be00b015bee9dc45efa
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-24 11:09:52 -0500

    tweaks

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -50,6 +50,8 @@ config.unix_domains = {
 	},
 }
 
+config.default_workspace = "0"
+
 wezterm.on("update-status", function(window, pane)
 	local colors = window:effective_config().resolved_palette
 	local workspace = window:active_workspace()
