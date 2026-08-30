15df4397bbd920fb4069e563eb9515e63dbdaf67
Jadon Brutcher <jadonwbrutcher@icloud.com>
2026-08-30 18:21:48 -0500

    add autostart for laptop

--- dot_config/hypr/autostart.lua
+++ dot_config/hypr/autostart.lua
@@ -1,2 +1,6 @@
 -- Extra autostart processes.
 -- o.launch_on_start("my-service")
+local machine = require("hypr.host")
+if machine.hostname == "omapad" then
+  o.launch_on_start("systemctl --user start kanata-homerow.service")
+end
